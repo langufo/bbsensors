018f155dcb381419e5689f302aad42a6f14d0a45
langufo <langufo@protonmail.com>
2019-09-19 09:05:18 +0200

    fixed NTC class for use with ADC class

--- HTM2500LF.cpp
+++ HTM2500LF.cpp
@@ -2,32 +2,33 @@
 
 #include "HTM2500LF.h"
 
-HTM2500LF::HTM2500LF(double nonprobedTempR,
-                     double nonprobedHumR, double probedHumR) {
+HTM2500LF::HTM2500LF(double tempDividerVoltage, double tempNonprobedR,
+                     double humNonprobedR, double humProbedR) {
 
-    HTM2500LF::nonprobedTempR = nonprobedTempR;
-    HTM2500LF::nonprobedHumR = nonprobedHumR;
-    HTM2500LF::probedHumR = probedHumR;
+    HTM2500LF::tempDividerVoltage = tempDividerVoltage;
+    HTM2500LF::tempNonprobedR = tempNonprobedR;
+    HTM2500LF::humNonprobedR = humNonprobedR;
+    HTM2500LF::humProbedR = humProbedR;
 }
 
-double HTM2500LF::getTemp(double adcValue) {
+double HTM2500LF::getTemp(double voltage) {
     const double a = 8.54942e-4;
     const double b = 2.57305e-4;
     const double c = 1.65368e-7;
 
-    double probedTempR = nonprobedTempR * adcValue/(4096-adcValue);
+    double probedTempR = tempNonprobedR * voltage/(tempDividerVoltage-voltage);
 
     return 1 / (a + b*log(probedTempR) + c*pow(log(probedTempR), 3)) - 273.15;
 }
 
-double HTM2500LF::getHum(double adcValue, double temp) {
+double HTM2500LF::getHum(double voltage, double temp) {
     const double a = -1.9206e-9;
     const double b = 1.437e-5;
     const double c = 3.421e-3;
     const double d = -12.4;
     const double e = 2.4e-3;
 
-    double voltage = 1800 * adcValue/4096 * (1+nonprobedHumR/probedHumR);  // millivolt
-    
+    voltage = voltage*1e3 * (1+humNonprobedR/humProbedR);  // millivolt
+
     return (a*pow(voltage, 3) + b*pow(voltage, 2) + c*voltage + d) / (1 + e*(temp-23));
 }
--- HTM2500LF.h
+++ HTM2500LF.h
@@ -6,49 +6,57 @@ public:
 
     /**
      * Costruttore.
-     * @param nonprobedTempR Resistenza in serie all'NTC; espressa in ohm.
-     * @param nonprobedHumR Resistenza del partitore per RH collegata al 
+     * @param tempDividerVoltage Tensione in volt applicata ai capi del 
+     *                           partitore per la misura della temperatura.
+     * @param tempNonProbedR Resistenza in serie all'NTC; espressa in ohm.
+     * @param humNonprobedR Resistenza del partitore per RH collegata al 
      *                      sensore; espressa in ohm.
-     * @param probedHumR Resistenza del partitore per RH collegata a terra; 
-     *                   espressa in ohm.
+     * @param humProbedHumR Resistenza del partitore per RH collegata a terra; 
+     *                      espressa in ohm.
      */
-    HTM2500LF(double nonprobedTempR, double nonProbedHumR, double probedHumR);
+    HTM2500LF(double tempDividerVoltage, double nonprobedTempR, 
+              double nonProbedHumR, double probedHumR);
 
     /**
      * Effettua una lettura della temperatura e ne restituisce il risultato in 
      * gradi celsius.
-	 * @param adcValue La lettura dell'ADC per l'NTC.
+     * @param voltage Tensione in volt per l'NTC.
      * @return La temperatura attuale in gradi celsius.
      */
-    double getTemp(double adcValue);
+    double getTemp(double voltage);
 
     /**
      * Effettua una lettura della temperatura e ne restituisce il risultato in 
      * punti percentuali.
-     * @param adcValue La lettura dell'ADC per il sensore di umidità.
+     * @param voltage Tensione in volt per il sensore di umidità.
      * @param temp Valore di temperatura in gradi celsius per cui correggere la 
-	 *             lettura di RH; se non è indicato non viene apportata alcuna 
-	 *             correzione per la temperatura.
-     * @return L'umidità relativa attuale in gradi celsius.
+     *             lettura di RH; se non è indicato non viene apportata alcuna 
+     *             correzione per la temperatura.
+     * @return L'umidità relativa attuale in punti percentuali.
      */
-    double getHum(double adcValue, double temp = 23);
+    double getHum(double voltage, double temp = 23);
 
 private:
 
     /**
-     * La resistenza in serie all'NTC; espressa in ohm.
+     * Tensione in volt applicata al partitore con l'NTC.
      */
-    double nonprobedTempR;
+    double tempDividerVoltage;
 
     /**
-     * La resistenza del partitore per RH collegata al sensore; espressa in ohm.
+     * Resistenza in ohm in serie all'NTC.
      */
-    double nonprobedHumR;
+    double tempNonprobedR;
 
     /**
-     * La resistenza del partitore per collegata a terra; espressa in kiloohm.
+     * Resistenza in ohm del partitore per RH collegata al sensore.
      */
-    double probedHumR;
+    double humNonprobedR;
+
+    /**
+     * Resistenza in ohm del partitore per collegata a terra.
+     */
+    double humProbedR;
 };
 
 #endif
